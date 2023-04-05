df0f5b4a3628dc5f708048fe0b1222c0aef0d36b
deltrece <118209787+deltrece@users.noreply.github.com>
2023-04-04 22:33:46 -0400

    Update
    Menu links work.  Scrolling do not.

--- js/app.js
+++ js/app.js
@@ -37,18 +37,6 @@ const pageBody = document.body;
  * ? Viewport Helper Function (Note:repitive for the viewport)
 */
 
-/*
-function isInViewport(element) {  //this is a helper function
-  const rect = element.getBoundingClientRect();
-  return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-}
-*/
-
 /**
  * ? End Helper Functions Above
  * ! Begin Main Functions Below
@@ -56,10 +44,9 @@ function isInViewport(element) {  //this is a helper function
 */
 
 /** 
-* * build the nav// active state w/ css//create a funtion using template literals that returns a new element//
+* * build the nav
 */
 const navBarList = document.getElementById("navbar__list"); // <unordered list> element located in <nav> element located in <header> element
-//const navMenu = document.getElementsByClassName("menu__link"); // class label in css file
 const navBarMenu  = document.getElementsByClassName("navbar__menu"); // class label in css file
 const sections = document.querySelectorAll("section");  // <section> element in html file (four of them)
 
@@ -70,66 +57,75 @@ sections.forEach((section)=> { // <--callback function
   const linkName = document.createElement("a"); // anchor link created
   linkName.innerHTML = `${section.dataset.nav}`; // text of section
   linkName.setAttribute("href", '#'+section.id); // link to section 
+  linkName.classList.add("menu__link");
   listItem.appendChild(linkName); // add to nav menu
-
-  // section.addEventListener("click", (event) => { // Adding a "click" event listener per link
-  //   event.preventDefault();  //prevents default behavior
-  //   linkName = event.target.dataset.nav; // Targeting each "data-block" dataset per clicked link
-  //   scrollTo(section.id);
-  // });
+  // need onclick event listener {behavior: "smooth"}
 	});
    
-//document.addEventListener("scroll", function() { makeActive();});
-
-// scrollTo(()=>{
-//   linkName.addEventListener("click", function());
-//   behavior: "smooth";
-// })
 
 /** 
 * * Add class 'active' to section when near top of viewport 
 */
-// const container = document.querySelectorAll("landing__container"); // class label in <div> section
-// const active = document.getElementsByClassName("your-active-class"); // class label in <section> element
-// const div = document.querySelectorAll('div'); // div element
+const active = document.getElementsByClassName("your-active-class"); // class label in <section> element
+const div = document.querySelectorAll('div'); // div element
 
-// function makeActive(){
-//   for(const section of sections) {
-//       const div = section.getBoundingClientRect();
-//       if (div.top <= 150 && div.bottom >= 150) {
-//       //apply active state on current section and corresponding Nav link
-//       section.landing__container.add("your-active-class");
-//       document.querySelector(`${'#'+section.id}`).landing__container.add("active");
-//       } else {
-//       //Remove active state from other section and corresponding Nav link
-//       section.landing__container.remove("your-active-class");
-//       document.querySelector(`${'#'+section.id}`).landing__container.remove("active");
-//       }
-//    }
-// }
+function makeActive(){
+  for(const section of sections) {
+      const div = section.getBoundingClientRect();
+      if (div.top <= 150 && div.bottom >= 150) {
+      //apply active state on current section and corresponding Nav link
+      section.classList.add("your-active-class");
+      document.querySelector(`${'#'+section.id}`).classList.add("active");
+      } else {
+      //Remove active state from other section and corresponding Nav link
+      section.classList.remove("your-active-class");
+      document.querySelector(`${'#'+section.id}`).classList.remove("active");
+      }
+   }
+}
 
-// window.addEventListener("scroll", function() {
-// 	div.forEach(function(div) {
-// 		const bounds = div.getBoundingClientRect();
-// 		const isInViewport = bounds.top >= 0
-// 			&& bounds.left > 0
-// 			&& bounds.right <= window.innerWidth
-// 			&& bounds.bottom <= window.innerWidth;
+window.addEventListener("scroll", function() {
+	div.forEach(function(div) {
+		const bounds = div.getBoundingClientRect();
+		const isInViewport = bounds.top >= 0
+			&& bounds.left > 0
+			&& bounds.right <= window.innerWidth
+			&& bounds.bottom <= window.innerWidth;
 
-// 		if (isInViewport) {
-// 			div.landing__container.add("your-active-class");
-// 		} else {
-// 			div.landing__container.remove("your-active-class");
-// 		}
-// 	});
-// })
-
-
+		if (isInViewport) {
+			div.classList.add("your-active-class");
+		} else {
+			div.classList.remove("your-active-class");
+		}
+	});
+})
 
 /** 
 * * Scroll to anchor ID using scrollTo event //onclick/ add click event listener//when I build the anchor link
 */ 
+const container = document.querySelectorAll("landing__container"); // class label in <div> section
 
+// document.querySelectorAll("section").forEach(anchor => {
+//   anchor.addEventListener("click, function"(e){
+//     e.preventDefault();
+//     document.querySelector(this.getAttribute("section")).scrollIntoView({
+//         behavior : "smooth";
+//     });
+//   });
+// });
+
+// (window).scroll(function() {
+//   ("section").each(function(){
+//      section = (this).offset().top;
+//      section = (window).scrollTop();
+//      onclick.preventDefault();
+//      section.scrollIntoView({behavior: "smooth"});
+     
+//   });
+// })
+
+// onclick.preventDefault();
+// section.scrollIntoView({ behavior: "smooth" });
 
 /**
  * ! End Main Functions Above
