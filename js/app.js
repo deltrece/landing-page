1869533550fcbdc871c6896b839bf536c2173eb3
ptums <ptumulty923@gmail.com>
2023-04-04 20:26:07 -0400

    added some comments

--- js/app.js
+++ js/app.js
@@ -1,28 +1,33 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ *
+ */
 
 /**
  * Comments should be present at the beginning of each procedure and class.
  * Great to have comments before crucial code sections within the procedure.
-*/
+ */
 
 /**
  * ! Define Global Variables Below
- * 
-*/
+ *
+ */
 // global functions are methods of the global object:
+
+/**
+ *
+ * You can probably remove this console message haha
+ */
 console.log("Wasssuuuppp!!!");
 
 const pageBody = document.body;
@@ -30,12 +35,17 @@ const pageBody = document.body;
 /**
  * ! End Global Variables Above
  * ? Start Helper Functions Below
- * 
-*/
+ *
+ */
 
 /**
  * ? Viewport Helper Function (Note:repitive for the viewport)
-*/
+ */
+
+/**
+ *
+ * If the code below isn't going to be used. You can remove it.
+ */
 
 /*
 function isInViewport(element) {  //this is a helper function
@@ -52,33 +62,52 @@ function isInViewport(element) {  //this is a helper function
 /**
  * ? End Helper Functions Above
  * ! Begin Main Functions Below
- * 
-*/
+ *
+ */
 
-/** 
-* * build the nav// active state w/ css//create a funtion using template literals that returns a new element//
-*/
+/**
+ * * build the nav// active state w/ css//create a funtion using template literals that returns a new element//
+ */
 const navBarList = document.getElementById("navbar__list"); // <unordered list> element located in <nav> element located in <header> element
-//const navMenu = document.getElementsByClassName("menu__link"); // class label in css file
-const navBarMenu  = document.getElementsByClassName("navbar__menu"); // class label in css file
-const sections = document.querySelectorAll("section");  // <section> element in html file (four of them)
 
-sections.forEach((section)=> { // <--callback function
+// You can probably remove this navMenu variable since it is commented out and not being used.
+//const navMenu = document.getElementsByClassName("menu__link"); // class label in css file
+
+const navBarMenu = document.getElementsByClassName("navbar__menu"); // class label in css file
+const sections = document.querySelectorAll("section"); // <section> element in html file (four of them)
+
+sections.forEach((section) => {
+  // <--callback function
   const listItem = document.createElement("li"); // create list element
-  navBarList.append(listItem);  // add li element to ul element
+  navBarList.append(listItem); // add li element to ul element
 
   const linkName = document.createElement("a"); // anchor link created
   linkName.innerHTML = `${section.dataset.nav}`; // text of section
-  linkName.setAttribute("href", '#'+section.id); // link to section 
+  linkName.setAttribute("href", "#" + section.id); // link to section
+
+  /**
+   * I don't remember if it is in the directions or not, but I think the a href in
+   * navigation need to have the menu__link class.
+   * So I added it for you
+   **/
+  linkName.classList.add("menu__link");
   listItem.appendChild(linkName); // add to nav menu
 
+  /**
+   *
+   *  The commented code below can be removed
+   */
   // section.addEventListener("click", (event) => { // Adding a "click" event listener per link
   //   event.preventDefault();  //prevents default behavior
   //   linkName = event.target.dataset.nav; // Targeting each "data-block" dataset per clicked link
   //   scrollTo(section.id);
   // });
-	});
-   
+});
+
+/**
+ *
+ *  You can remove all this commented out code, since it is not being used.
+ */
 //document.addEventListener("scroll", function() { makeActive();});
 
 // scrollTo(()=>{
@@ -86,9 +115,9 @@ sections.forEach((section)=> { // <--callback function
 //   behavior: "smooth";
 // })
 
-/** 
-* * Add class 'active' to section when near top of viewport 
-*/
+/**
+ * * Add class 'active' to section when near top of viewport
+ */
 // const container = document.querySelectorAll("landing__container"); // class label in <div> section
 // const active = document.getElementsByClassName("your-active-class"); // class label in <section> element
 // const div = document.querySelectorAll('div'); // div element
@@ -124,22 +153,25 @@ sections.forEach((section)=> { // <--callback function
 // 	});
 // })
 
-
-
-/** 
-* * Scroll to anchor ID using scrollTo event //onclick/ add click event listener//when I build the anchor link
-*/ 
-
+/**
+ * * Scroll to anchor ID using scrollTo event //onclick/ add click event listener//when I build the anchor link
+ */
 
 /**
  * ! End Main Functions Above
  * ? Begin Events Below
- * 
-*/
-// Build menu 
+ *
+ */
+// Build menu
 
 // Scroll to section on link click
 
 // Set sections as active
 
-
+/***
+ *
+ *  Overall great job!!
+ *  After you clean up all the unused code. I know there are 2 other things you need to do.
+ *  1. Make the navigation link have "your-active-class" class if the related section is in the view port
+ *  2. Add a scroll effect that will slide to the section when the user clicks the link
+ */
